b46187c817e4b2220649b7a5b0006b15c65ba757
Gabriel Cesario <gabriel.cesario@uel.br>
2025-11-13 10:47:58 -0300

    correcao da descricao do arquivo .geo

--- src/geo.h
+++ src/geo.h
@@ -11,15 +11,14 @@
 #include "texto.h"
 #include "fila.h"
 
-typedef void* Chao;
 
 /*
 Arquivo .h relacionado a leitura do arquivo geo
 */
 
 /// @brief Abre o arquivo .geo no modo leitura e processa as todas as informacoes nele contidas, e assim adiciona-as em uma fila chamada chao
-/// @param nomeGeo Nome do arquivo .geo que sera processado
-/// @param nomeSvg Nome do arquivo svg onde serao printadas as formas do chao
+/// @param Geo Nome do arquivo .geo que sera processado
+/// @param svgGeo Nome do arquivo svg onde serao printadas as formas do chao
 /// @param nformas Valor passado por referencia que guarda a quantidade total de formas no chao inicial
 /// @param ts O estilo do texto
 /// @return retorna um ponteiro para a fila 'chao'
